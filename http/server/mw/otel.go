5cac6d2e4c3f1d3918293f82d740c1e4bae774ab
Gaiaz Iusipov <g.iusipov@gmail.com>
2026-02-11 07:14:05 +0800

    Revert "feat(http/server): Experimental no request pattern on metrics"
    This reverts commit 80beca856f11e4137fbe71650cad88e491038085.

--- http/server/mw/otel.go
+++ http/server/mw/otel.go
@@ -2,8 +2,10 @@ package httpservermw
 
 import (
 	"net/http"
+	"strings"
 
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	semconv "go.opentelemetry.io/otel/semconv/v1.39.0"
 	"go.opentelemetry.io/otel/trace"
 
 	httpheader "github.com/gaiaz-iusipov/go-common/http/header"
@@ -20,23 +22,22 @@ func (mw OTEL) Handler() func(next http.Handler) http.Handler {
 			otelhttp.NewMiddleware(mw.Operation,
 				otelhttp.WithSpanNameFormatter(mw.spanNameFormatter),
 				otelhttp.WithServerName(mw.ServerName),
-				// otelhttp.WithMetricAttributesFn(mw.metricAttributesFn),
 			),
-			// mw.routeMetrics,
+			mw.routeMetrics,
 			mw.exportTraceID,
 		}.Handle(next)
 	}
 }
 
-// func (OTEL) routeMetrics(next http.Handler) http.Handler {
-// 	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-// 		if idx := strings.IndexByte(req.Pattern, '/'); idx >= 0 {
-// 			labeler, _ := otelhttp.LabelerFromContext(req.Context())
-// 			labeler.Add(semconv.HTTPRoute(req.Pattern[idx:]))
-// 		}
-// 		next.ServeHTTP(rw, req)
-// 	})
-// }
+func (OTEL) routeMetrics(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		if idx := strings.IndexByte(req.Pattern, '/'); idx >= 0 {
+			labeler, _ := otelhttp.LabelerFromContext(req.Context())
+			labeler.Add(semconv.HTTPRoute(req.Pattern[idx:]))
+		}
+		next.ServeHTTP(rw, req)
+	})
+}
 
 func (OTEL) exportTraceID(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
@@ -56,10 +57,3 @@ func (OTEL) spanNameFormatter(operation string, req *http.Request) string {
 	}
 	return operation + " " + req.Pattern
 }
-
-// func (OTEL) metricAttributesFn(req *http.Request) []attribute.KeyValue {
-// 	if idx := strings.IndexByte(req.Pattern, '/'); idx >= 0 {
-// 		return []attribute.KeyValue{semconv.HTTPRoute(req.Pattern[idx:])}
-// 	}
-// 	return nil
-// }
