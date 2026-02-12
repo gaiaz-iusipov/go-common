270fad16ecabc935cbf83352ca56409dffb473d5
Gaiaz Iusipov <g.iusipov@gmail.com>
2026-02-12 19:27:41 +0800

    feat(http/server): Use WithMetricAttributesFn

--- http/server/mw/otel.go
+++ http/server/mw/otel.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel/attribute"
 	semconv "go.opentelemetry.io/otel/semconv/v1.39.0"
 	"go.opentelemetry.io/otel/trace"
 
@@ -22,21 +23,11 @@ func (mw OTEL) Handler() func(next http.Handler) http.Handler {
 			otelhttp.NewMiddleware(mw.Operation,
 				otelhttp.WithSpanNameFormatter(mw.spanNameFormatter),
 				otelhttp.WithServerName(mw.ServerName),
+				otelhttp.WithMetricAttributesFn(mw.metricAttributesFn),
 			),
-			mw.routeMetrics,
 			mw.exportTraceID,
 		}.Handle(next)
 	}
-}
-
-func (OTEL) routeMetrics(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		if idx := strings.IndexByte(req.Pattern, '/'); idx >= 0 {
-			labeler, _ := otelhttp.LabelerFromContext(req.Context())
-			labeler.Add(semconv.HTTPRoute(req.Pattern[idx:]))
-		}
-		next.ServeHTTP(rw, req)
-	})
 }
 
 func (OTEL) exportTraceID(next http.Handler) http.Handler {
@@ -56,4 +47,11 @@ func (OTEL) spanNameFormatter(operation string, req *http.Request) string {
 		return req.Pattern
 	}
 	return operation + " " + req.Pattern
+}
+
+func (OTEL) metricAttributesFn(req *http.Request) []attribute.KeyValue {
+	if idx := strings.IndexByte(req.Pattern, '/'); idx >= 0 {
+		return []attribute.KeyValue{semconv.HTTPRoute(req.Pattern[idx:])}
+	}
+	return nil
 }
